32619fad8250f3f417b13bb25b42ff4d1f973e62
Andrew Forward <aforward@hey.com>
2020-08-21 12:22:10 -0400

    Add deal method

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+
+	hand, remainingCards := deal(cards, 3)
+
+	fmt.Println("My Hand")
+	hand.print()
+
+	fmt.Println("Remaining Deck")
+	remainingCards.print()
 }
