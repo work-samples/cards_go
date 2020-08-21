4cf2fe652780d3e234e59ec29285e71cf9b18290
Andrew Forward <aforward@hey.com>
2020-08-21 12:47:12 -0400

    Add decks.saveToFile

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
+	cards.saveToFile("cards.csv")
+	// hand, remainingCards := deal(cards, 3)
 
-	hand, remainingCards := deal(cards, 3)
+	// fmt.Println("My Hand")
+	// fmt.Println(hand.toString())
 
-	fmt.Println("My Hand")
-	fmt.Println(hand.toString())
-
-	fmt.Println("Remaining Deck")
-	fmt.Println(remainingCards.toString())
+	// fmt.Println("Remaining Deck")
+	// fmt.Println(remainingCards.toString())
 }
